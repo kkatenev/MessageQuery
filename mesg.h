7fecf2a904910830decb73f73edd9f668acbe473
kkatenev <kkatenev@mail.ru>
2022-08-09 23:22:27 +0700

    initial commit

--- mesg.h
+++ mesg.h
@@ -3,6 +3,6 @@
 #define PERM 0666
 typedef struct our_msgbuf //имя структуры
 {
-long mtype; //тип сообщения
-char buff[MAXBUFF] ;
-}Message; //тип структуры
+    long mtype; //тип сообщения
+    char buff[MAXBUFF] ;
+} Message; //тип структуры
